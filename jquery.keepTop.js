2016d481179e45e00d403aa1299469acf9d8f229
Kim Doberstein <dobe0002@umn.edu>
2011-08-09 11:32:34 -0500

    First commit

---
+++ jquery.keepTop.js
@@ -0,0 +1,50 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// jquery.keepTop.js
+// version 1.0.0 
+// 08/09/2011
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+(function( $ ){
+
+	$.fn.keepTop = function( options ) {  
+		//default settings
+		var settings={
+			'init':true,
+			'zindex':400,
+			'createPrint':false,
+			'divHolder':true,
+			'printClass':'banner-print',
+			'rulerID':'keepTopRuler',
+			'left':0,
+			'fitWidth':true
+		};  
+		return this.each(function() {
+			if ( options ) { $.extend( settings, options ); }
+			
+			var $this = $(this);
+			if(settings.init==true){	
+		
+				if(settings.createPrint==true)$this.clone().removeAttr('id').addClass(settings.printClass).insertAfter($this);
+				if(settings.divHolder==true)$this.after("<div style='height:"+$this.outerHeight()+"px'></div>");
+				
+				$this.after("<div style='height:0px' id='"+settings.rulerID+"'></div>");
+				$this.css({'right':'0','position':'fixed','zindex':settings.zindex});
+				settings.init=false;
+				$(window).scroll(function(){$this.keepTop(settings);}).resize(function(){$this.keepTop(settings);});
+			
+			}
+			var newTop=$('#'+settings.rulerID).offset().top-$(window).scrollTop();
+			if(newTop<0) newTop=0;
+			if(settings.fitWidth==true){
+				var left=$('#'+settings.rulerID).offset().left;
+				var width=$('#'+settings.rulerID).width()-($this.outerWidth()-$this.width());
+				$this.css({'width':width+"px"});
+			}
+			else var left=settings.left;
+			$this.css({'top':newTop+"px",'left':left+"px"});
+	
+		}); // End each
+
+	};// end function
+})( jQuery );
